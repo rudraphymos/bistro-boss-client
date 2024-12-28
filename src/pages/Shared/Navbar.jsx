ffd359d49ddc783f05bdf36dd34f7099eb74b95a
Rudra <rudra.phymos@gmail.com>
2024-12-28 20:16:01 +0530

    react responsive carousel added

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     </>
     
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar fixed z-10 bg-opacity-50 bg-[#151515] max-w-screen-xl text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
