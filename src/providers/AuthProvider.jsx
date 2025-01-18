776f6ba3ab4ccfb9eea86afc69e0875aaa3d87b8
Rudra <rudra.phymos@gmail.com>
2025-01-18 16:35:22 +0530

    jwt added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { createContext } from "react";
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
@@ -11,6 +12,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -43,12 +45,23 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
-            console.log("current user:", currentUser);
+            if (currentUser) {
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         });
 
         return () => unsubscribe();;
-    }, []);
+    }, [axiosPublic]);
 
     const authInfo = {
         user,
--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,20 +1,23 @@
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const PrivateRoute = ({ children }) => {
+const AdminRoute = (children) => {
 
-    const { user, loading } = useAuth();
+    const [user, loading] = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+
     const location = useLocation();
 
-    if (loading) {
+    if (loading || isAdminLoading) {
         return <span className="loading loading-infinity loading-lg"></span>
     }
 
-    if (user) {
+    if (user && isAdmin) {
         return children;
     }
 
     return <Navigate to="/login" state={{from: location}} replace></Navigate>;
 };
 
-export default PrivateRoute;
+export default AdminRoute;
